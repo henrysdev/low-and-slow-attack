e53b086bf479e6e56897d139a0b9e412ab1377dd
Henry Warren <henrysdev@gmail.com>
2017-08-08 22:24:02 -0500

    Added connection timeout

--- connect.c
+++ connect.c
@@ -6,29 +6,48 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int tcp_socket;
 
-int* initConnection(char* target)
+int * initConnection(char* target)
 {
 	struct sockaddr_in svr_address;
-
-	// ERROR CHECKING NECESSARY
-	tcp_socket = socket(AF_INET, SOCK_STREAM, 0); 
+	int * tcp_socket = malloc(sizeof(int));
+	*tcp_socket = socket(AF_INET, SOCK_STREAM, 0); 
+	fcntl(*tcp_socket, F_SETFL, O_NONBLOCK);
+	fd_set fdset;
+	struct timeval tv;
 
 	svr_address.sin_family = AF_INET;
 	svr_address.sin_port = htons(80);
 	svr_address.sin_addr.s_addr = inet_addr(target);
 	memset(&(svr_address.sin_zero), '\0', 8);
 
-	if(0 > connect(tcp_socket, (struct sockaddr *)&svr_address, sizeof(struct sockaddr)))
+	connect(*tcp_socket, (struct sockaddr *)&svr_address, sizeof(struct sockaddr));
+
+	FD_ZERO(&fdset);
+	FD_SET(*tcp_socket, &fdset);
+	tv.tv_sec = 10;
+	tv.tv_usec = 0;
+
+	if (select(*tcp_socket + 1, NULL, &fdset, NULL, &tv) == 1)
 	{
-		return NULL;
+		int so_error;
+		socklen_t len = sizeof(so_error);
+
+		getsockopt(*tcp_socket, SOL_SOCKET, SO_ERROR, &so_error, &len);
+
+		if (!so_error)
+		{
+			return tcp_socket;
+		}
 	}
-	return &tcp_socket;
+	// return failed connection if this point is reached
+	free(tcp_socket);
+	return NULL;
 }
 
 int main(int argc, char* argv[])
@@ -44,7 +63,7 @@ int main(int argc, char* argv[])
 	int num_conns = atoi(argv[2]);
 
 	int* open_conns[num_conns];
-	int n = 0;
+	int actv_cnt = 0;
 	int i;
 	for(i = 0; i < num_conns; i++)
 	{
@@ -56,10 +75,13 @@ int main(int argc, char* argv[])
 		else
 		{
 			printf("%s\n", "connection established");
-			open_conns[n] = resp;
-			++n;
+			open_conns[actv_cnt] = resp;
+			++actv_cnt;
 		}
 	}
 
+	int* active_conns[actv_cnt];
+	memcpy(active_conns, open_conns, sizeof(active_conns) );
+	
 	return 0;
 }
